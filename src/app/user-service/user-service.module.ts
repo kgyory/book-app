f1558cd65c48a495a8686f56b8f1ddc11db84e2c
Bernhard Ninaus <ninausbernhard@gmail.com>
2022-01-30 01:12:26 +0100

    Created user-card ui component.

--- src/app/user-service/user-service.module.ts
+++ src/app/user-service/user-service.module.ts
@@ -4,10 +4,12 @@ import { SharedModule } from '../shared/shared.module';
 import { UserSearchComponent } from './feature/user-search/user-search.component';
 import { AddUserComponent } from './feature/add-user/add-user.component';
 import { UserServiceRoutingModule } from './user-service-routing.module';
+import { UserCardComponent } from './ui/user-card/user-card.component';
 
 @NgModule({
     declarations: [
         UserSearchComponent,
+        UserCardComponent,
         AddUserComponent
     ],
     imports: [
